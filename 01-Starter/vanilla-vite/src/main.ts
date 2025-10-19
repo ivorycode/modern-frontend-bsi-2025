788fdbe9d2087c42b380eba136f8a1554ca1a8a8
Jonas Bandi <dev@jonasbandi.net>
2025-10-20 01:30:17 +0200

    Styling via tailwind

--- 01-Starter/vanilla-vite/src/main.ts
+++ 01-Starter/vanilla-vite/src/main.ts
@@ -4,18 +4,20 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="max-w-[1280px] mx-auto px-8 py-8 text-center">
+    <div class="flex justify-center items-center gap-4">
+      <a href="https://vite.dev" target="_blank" >
+        <img src="${viteLogo}" class="h-36 p-6 duration-300 hover:drop-shadow-vite-glow" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="h-36 p-6 duration-300 hover:drop-shadow-typescript-glow" alt="TypeScript logo" />
+      </a>
     </div>
-    <p class="read-the-docs">
+    <h1 class="text-[3.2em] font-bold leading-[1.1]">Vite + TypeScript</h1>
+    <div class="p-8">
+      <button id="counter" type="button" class="rounded-lg border border-gray-300 px-5 py-2.5 cursor-pointer"></button>
+    </div>
+    <p class="text-text-muted">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
