e41368a0ad966aaa14b31fb8527a35821437a0d2
Jonas Bandi <dev@jonasbandi.net>
2025-10-27 20:34:18 +0100

    Web Socket example

--- 04-web-platform/02-websocket/server.js
+++ 04-web-platform/02-websocket/server.js
@@ -1,7 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const WebSocket = require('ws');
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { WebSocketServer } from 'ws';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 8080;
 
@@ -30,7 +34,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Create WebSocket server attached to HTTP server
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
