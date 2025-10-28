2a219a06bdf893cda8fa7f619f06830f68c3ff93
Jonas Bandi <dev@jonasbandi.net>
2025-10-28 13:45:27 +0100

    spaghetti exercise

--- 02-Language/92-Spaghetti/app/module2.js
+++ 02-Language/92-Spaghetti/app/module2.js
@@ -1,11 +1,11 @@
 var MODULE_PREFIX = "module2_";
 
-function storeDate() {
+function loadValues() {
   localStorage.setItem(MODULE_PREFIX + "val1", new Date().toDateString());
   localStorage.setItem(MODULE_PREFIX + "val2", new Date().toTimeString());
 }
 
-function loadDate() {
+function storeValues() {
   var val1 = localStorage.getItem(MODULE_PREFIX + "val1");
   var val2 = localStorage.getItem(MODULE_PREFIX + "val2");
 
@@ -14,10 +14,10 @@ function loadDate() {
 
 /* We extend the page with the new feature: */
 $("#storeButton2").click(function () {
-  storeDate();
+  storeValues();
 });
 $("#loadButton2").click(function () {
-  var settings = loadDate();
+  var settings = loadValues();
   $("#txtValue3").text(settings[0]);
   $("#txtValue4").text(settings[1]);
 });
